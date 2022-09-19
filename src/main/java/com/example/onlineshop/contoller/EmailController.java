11cd742f26b4dc2c5c2dcc670dd98361f31c5c5b
Mr. X <mohebmoallem1376@gmail.com>
2022-09-20 00:19:23 +0530

    edit email service

--- src/main/java/com/example/onlineshop/contoller/EmailController.java
+++ src/main/java/com/example/onlineshop/contoller/EmailController.java
@@ -1,10 +1,8 @@
 package com.example.onlineshop.contoller;
 
-import com.example.onlineshop.model.User;
 import com.example.onlineshop.service.EmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +13,12 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("email/forget-password")
-    public ResponseEntity<String> sendPasswordEmail(@RequestParam String email){
+    public ResponseEntity<String> sendPasswordEmail(@RequestParam String email) {
         return ResponseEntity.ok(emailService.sendForgottenPasswordEmail(email));
     }
 
     @PostMapping("email/resend-verifying-code")
-    public ResponseEntity<String> resendPasswordEmail(@RequestParam String email){
+    public ResponseEntity<String> resendPasswordEmail(@RequestParam String email) {
         return ResponseEntity.ok(emailService.resendVerifyingEmil(email));
     }
 }
