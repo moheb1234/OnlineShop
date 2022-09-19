e54b5e1eac7a50e9b162cd53c2af017590b01a1e
Mr. X <mohebmoallem1376@gmail.com>
2022-09-20 00:02:46 +0530

    improve filter product

--- src/main/java/com/example/onlineshop/repository/ProductRepository.java
+++ src/main/java/com/example/onlineshop/repository/ProductRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("select p from Product p where (?1 ='' or p.productCategories = ?1) and (?2='' or lower(p.name) like lower(concat('%',?2,'%'))) and (p.inventory >= ?3)")
+    @Query("select p from Product p where (?1 ='' or p.productCategories = ?1) and (?2='' or lower(p.name) like lower(concat('%',?2,'%'))) and (p.inventory.number >= ?3)")
     List<Product> filtersAll(String productCategories, String name, int inventory, Sort sort);
 
     //for unit test
