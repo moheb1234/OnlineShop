f54d1c8d6169c79a0be9c0412c0719f64272a513
Mr. X <mohebmoallem1376@gmail.com>
2022-09-29 11:55:55 +0530

    improve security deprecation

--- src/main/java/com/example/onlineshop/security/SecurityConfig.java
+++ src/main/java/com/example/onlineshop/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.example.onlineshop.security;
 import com.example.onlineshop.enums.RoleName;
 import com.example.onlineshop.security.jwt.JwtTokenFilter;
 import com.example.onlineshop.service.UserService;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,9 +12,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -23,22 +23,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 
-@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@Deprecated
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
+@RequiredArgsConstructor
+public class SecurityConfig {
     private final JwtTokenFilter jwtTokenFilter;
 
-    public SecurityConfig(UserService userService, JwtTokenFilter jwtTokenFilter) {
-        this.userService = userService;
-        this.jwtTokenFilter = jwtTokenFilter;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().configurationSource(corsConfigurationSource()).and().authorizeRequests()
                 .antMatchers(adminAuthorizeHttp()).hasAnyAuthority(RoleName.ADMIN.toString())
                 .antMatchers(userAuthorizeHttp()).hasAnyAuthority(RoleName.USER.toString())
@@ -48,14 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().logout()
                 .and().csrf().disable();
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-        return provider;
+        return http.build();
     }
 
     @Bean
@@ -68,10 +54,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider());
-    }
 
     public String[] adminAuthorizeHttp() {
         return new String[]{"/product/**", "/role/**", "user/**"};
@@ -82,6 +64,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     public String[] permitAllAuthorizeHttp() {
-        return new String[]{"/products/**", "/signing", "/signup","/user/verifying/**","/email/forget-password/**","/email/resend-verifying-code/**"};
+        return new String[]{"/products/**", "/signing", "/signup", "/user/verifying/**", "/email/forget-password/**", "/email/resend-verifying-code/**"};
     }
 }
